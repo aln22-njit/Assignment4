f43e28d3ff1a0eec7f653762ba788b1ebe6a66f9
Adam Nguyen <aln22@njit.edu>
2025-11-26 17:46:02 -0500

    Uploading outline of assignment

---
+++ frontend/assignment4-app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Body } from './components/body/body';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Body],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('assignment4-app');
+}
